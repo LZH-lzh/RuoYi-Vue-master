7a0d791e66ec4a60a7f6c39e3b044d8f75d3650f
lzhui <1481369299@qq.com>
2022-10-09 09:54:42 +0800

    by lzh-vuln 22.10.9 first commit v/c3

---
+++ ruoyi-vuln/src/main/java/Tt.java
@@ -0,0 +1,10 @@
+/**
+ * @Description: TODO
+ * @author: lzh
+ * @date: 2022年10月09日 9:54
+ */
+public class Tt {
+    public static void main(String[] args) {
+        System.out.println("vuln1");
+    }
+}
